9a468f6043cfd30d7790230462bce95d8c655728
stanolacko <stano.lacko@gmail.com>
2024-11-22 14:04:25 +0100

    0.1

---
+++ Wikicarpedia.i18n.magic.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Wikicarpedia.com specific hooks and improvements
+ *
+ * @file Wikicarpedia.i18n.magic.php
+ * @ingroup Wikicarpedia
+ * @author Bumsakalaka
+ */
+
+$magicWords = [];
+
+/**
+ * English
+ * @author Bumsakalaka
+ */
+$magicWords['en'] = [
+	'wicalist' => [ 0, 'wicalist' ],
+];
